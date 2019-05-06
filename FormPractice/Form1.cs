8f1a2d46b5818431d693080d509a3c384c86948c
Hannale Hylander <hjhylaner@gmail.com>
2019-05-06 09:30:28 -0500

    first commit

---
+++ FormPractice/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPractice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //96-122
+            while (true)
+            {
+
+                #region cemetery
+                
+                //string[] colors = { "blue", "red", "yellow", "purple", "green", "orange" };
+                //
+                //
+                //int blen = colors[0].Length;
+                //int rlen = colors[1].Length;
+                //int ylen = colors[2].Length;
+                //int plen = colors[3].Length;
+                //int glen = colors[4].Length;
+                //int olen = colors[5].Length;
+                //
+                //
+                //int Rindex = richTextBox1.Text.IndexOf("red");
+                //richTextBox1.Select(Rindex, rlen);
+                //richTextBox1.SelectionColor = Color.Red;
+                //
+                //int Bindex = richTextBox1.Text.IndexOf("blue");
+                //richTextBox1.Select(Bindex, blen);
+                //richTextBox1.SelectionColor = Color.Blue;
+                string[] alphabet = { "a", "b", "c", "d", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
+
+            }
+            //int a = alphabet[0].Length;
+            //int b = alphabet[1].Length;
+            //int c = alphabet[2].Length;
+            //int d = alphabet[3].Length;
+            //int f = alphabet[4].Length;
+            //int g = alphabet[5].Length;
+            //int h = alphabet[6].Length;
+            //int i = alphabet[7].Length;
+            //int j = alphabet[8].Length;
+            //int k = alphabet[9].Length;
+            //int l = alphabet[10].Length;
+            //int m = alphabet[11].Length;
+            //int n = alphabet[12].Length;
+            //int o = alphabet[13].Length;
+            //int p = alphabet[14].Length;
+            //int q = alphabet[15].Length;
+            //int r = alphabet[16].Length;
+            //int s = alphabet[17].Length;
+            //int t = alphabet[18].Length;
+            //int u = alphabet[19].Length;
+            //int v = alphabet[20].Length;
+            //int w = alphabet[21].Length;
+            //int x = alphabet[22].Length;
+            //int y = alphabet[23].Length;
+            //int z = alphabet[24].Length;
+            //
+            //richTextBox1.Text.ToLower();
+            //
+
+            for (int ii = 0; ii < RichTextBox1; i++) ;
+            
+             }
+            //int Aa = richTextBox1.Text.IndexOf("a");
+            //richTextBox1.Select(Aa, a);
+            //richTextBox1.SelectionColor = Color.Red;
+            //
+            //int Bb = richTextBox1.Text.IndexOf("b");
+            //richTextBox1.Select(Bb, b);
+            //richTextBox1.SelectionColor = Color.Chocolate;
+            //
+            //int Cc = richTextBox1.Text.IndexOf("c");
+            //richTextBox1.Select(Cc, c);
+            //richTextBox1.SelectionColor = Color.GreenYellow;
+            //
+            //int Dd = richTextBox1.Text.IndexOf("d");
+            //richTextBox1.Select(Dd, d);
+            //richTextBox1.SelectionColor = Color.Firebrick;
+            //
+           //// int Eelf = richTextBox1.Text.IndexOf("e");
+           //// richTextBox1.Select(Ee, e);
+           //// richTextBox1.SelectionColor = Color.DodgerBlue;
+            //
+            //int Ff = richTextBox1.Text.IndexOf("f");
+            //richTextBox1.Select(Ff, f);
+            //richTextBox1.SelectionColor = Color.DarkCyan;
+            //
+            //int Gg = richTextBox1.Text.IndexOf("g");
+            //richTextBox1.Select(Gg, g);
+            //richTextBox1.SelectionColor = Color.DarkRed;
+            //
+            //int Hh = richTextBox1.Text.IndexOf("h");
+            //richTextBox1.Select(Hh, h);
+            //richTextBox1.SelectionColor = Color.Gainsboro;
+            //
+            //int Ii = richTextBox1.Text.IndexOf("i");
+            //richTextBox1.Select(Ii, i);
+            //richTextBox1.SelectionColor = Color.Fuchsia;
+            //
+            //int Jj = richTextBox1.Text.IndexOf("j");
+            //richTextBox1.Select(Jj, j);
+            //richTextBox1.SelectionColor = Color.LightSalmon;
+            //
+            //int Kk = richTextBox1.Text.IndexOf("k");
+            //richTextBox1.Select(Kk, k);
+            //richTextBox1.SelectionColor = Color.Orange;
+            //
+            //int Ll = richTextBox1.Text.IndexOf("l");
+            //richTextBox1.Select(Ll, l);
+            //richTextBox1.SelectionColor = Color.Turquoise;
+            //
+            //int Mm = richTextBox1.Text.IndexOf("m");
+            //richTextBox1.Select(Mm, m);
+            //richTextBox1.SelectionColor = Color.Tomato;
+            //
+            //int Nn = richTextBox1.Text.IndexOf("n");
+            //richTextBox1.Select(Nn, n);
+            //richTextBox1.SelectionColor = Color.Sienna;
+            //
+            //int Oo = richTextBox1.Text.IndexOf("o");
+            //richTextBox1.Select(Oo, o);
+            //richTextBox1.SelectionColor = Color.Olive;
+            //
+            //int Pp = richTextBox1.Text.IndexOf("p");
+            //richTextBox1.Select(Pp, p);
+            //richTextBox1.SelectionColor = Color.Moccasin;
+            #endregion
+
+
+
+        }
+    }
+}
